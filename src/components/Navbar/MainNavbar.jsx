42497f811d5f5347e832472f9de354e2bc597dc1
zak <codedpills@gmail.com>
2021-05-14 22:58:58 +0000

    add Navbar

--- src/components/Navbar/MainNavbar.jsx
+++ src/components/Navbar/MainNavbar.jsx
@@ -1,29 +1,31 @@
-import React from 'react';
+import React from "react"
+import {Link} from 'gatsby'
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import Container from "react-bootstrap/Container"
+
+import "bootstrap/dist/css/bootstrap.min.css"
+
+import './mainNavbar.scss';
 
 const MainNavbar = () => {
-    return (
-        <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+  return (
+    <Navbar bg="light" expand="lg" className="main-navbar">
+      <Container>
+        <Navbar.Brand href="#home">
+            <span className="logo-concert">Concert</span>
+            <span className="logo-party">Party</span>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+          <Nav className="ml-auto">
+            <Link to="/concerts" className="nav-link">Featured Concerts</Link>
+            <Link to="/login" className="nav-link">Login</Link>
           </Nav>
-          <Form inline>
-            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
-      </Navbar>
-    );
+      </Container>
+    </Navbar>
+  )
 }
 
-export default MainNavbar;
+export default MainNavbar
