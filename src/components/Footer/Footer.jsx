22d2d1a367706e899bb4317fbd54b88097447b28
zak <codedpills@gmail.com>
2021-05-17 18:25:02 +0000

    create footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+import React from "react"
+import "bootstrap-icons/font/bootstrap-icons.css"
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <p>
+              &copy; Concert Party {new Date().getFullYear()}. All rights
+              reserved.
+            </p>
+          </div>
+          <div className="col">
+            <ul className="footer-links">
+              <li>
+                <a href="">
+                  <i className="bi-facebook"></i>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i className="bi-instagram"></i>
+                </a>
+              </li>
+              <li>
+                <a href="">
+                  <i className="bi-twitter"></i>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
