7a7ae02e7754e878af14ccd0e1ce2e98bba91a35
zak <codedpills@gmail.com>
2021-05-19 23:25:36 +0000

    render title and html from markdown file

--- src/components/About/WhyConcertParty.jsx
+++ src/components/About/WhyConcertParty.jsx
@@ -1,25 +1,26 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 import img from "../../images/hero/anthony-delanoix-hzgs56Ze49s-unsplash.jpg"
 
 const WhyConcertParty = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      markdownRemark {
+        frontmatter {
+          title
+        }
+        html
+      }
+    }
+  `)
+
+  console.log(data)
+
   return (
     <div className="why-concert-party">
-      <h4>Why Concert Party</h4>
-      <div>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-          eligendi molestiae dolores quas voluptates quo totam sit adipisci
-          laborum eum, maxime iste, quibusdam eveniet illo non ad asperiores
-          nam! Perspiciatis!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
-          eligendi molestiae dolores quas voluptates quo totam sit adipisci
-          laborum eum, maxime iste, quibusdam eveniet illo non ad asperiores
-          nam! Perspiciatis!
-        </p>
-      </div>
+      <h4>{data.markdownRemark.frontmatter.title}</h4>
+      <div dangerouslySetInnerHTML={{__html: data.markdownRemark.html}}></div>
       <img src={img} alt="about concert party" className="img-fluid" />
     </div>
   )
