77569babfb7f4723049df64b7ec23e573084b058
zak <codedpills@gmail.com>
2021-05-19 22:35:50 +0000

    configure gatsby-source-filesystem plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,6 +15,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
