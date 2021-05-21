3383574478ba73e09ac87f3a744e3b10bc8dc8d5
zak <codedpills@gmail.com>
2021-05-21 22:25:27 +0000

    create login wrapper

---
+++ src/components/Login/LoginWrapper.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+
+import './login.scss'
+
+const LoginWrapper = () => {
+  return (
+    <div className="login-wrapper">
+      <div className="row no-gutters">
+        <div className="col-5">bg image</div>
+        <div className="col">
+          <div className="login-form-section">
+            <div className="logo-area">
+              <span className="logo-concert">Concert</span>
+              <span className="logo-party">Party</span>
+            </div>
+            <div className="form-area">
+              <h4 className="text-center mb-4">Hello, old friend. Sign in</h4>
+              <br />
+              Form here
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginWrapper
