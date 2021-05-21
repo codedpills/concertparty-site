95d19bb2585209421c8619b2bb7b1c8cd1ab821a
zak <codedpills@gmail.com>
2021-05-21 16:11:26 +0000

    query concerts and generate pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,29 +1,30 @@
 const path = require("path")
 
-exports.createPages = async ({ grapghql, actions }) => {
+exports.createPages = async ({ graphql, actions }) => {
   const concertDetailsTemplate = path.resolve(
     `./src/templates/ConcertDetails.jsx`
   )
 
   // query slugs from Contentful
-  const result = await grapghql(`
+  const result = await graphql(`
     query {
-      allContentfulFeaturedconcerts {
-          nodes {
-              slug
-          } 
-      }
-  }`)
+        allContentfulFeaturedconcerts {
+            nodes {
+                slug
+            }
+        }
+    }
+  `)
 
   // create pages for each concert
-  const slugs = result.data.allContentfulFeaturedConcerts.nodes
-  slugs.map(({ slug }) => {
-    node.url = `/concerts/${slug}`
+  const nodes = result.data.allContentfulFeaturedconcerts.nodes;
+  nodes.map((node) => {
+      node.url = `/concerts/${node.slug}`
 
-    actions.createPage({
-      path: node.url,
-      component: concertDetailsTemplate,
-      context: { slug: node.slug },
-    })
+      actions.createPage({
+          path: node.url,
+          component: concertDetailsTemplate,
+          context: { slug: node.slug}
+      })
   })
 }
