9ebe94835eb066191f777d85a710c0ac114ea5b3
zak <codedpills@gmail.com>
2021-05-21 22:25:44 +0000

    render LoginWrapper

---
+++ src/pages/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Layout from '../components/Layout'
+import LoginWrapper from '../components/Login/LoginWrapper';
+
+const login = () => {
+    return (
+        <Layout>
+            <LoginWrapper />
+        </Layout>
+    );
+}
+
+export default login;
