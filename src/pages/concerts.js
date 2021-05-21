f7b5bea62144be087e2abd3186a81b28f7e921a6
zak <codedpills@gmail.com>
2021-05-21 09:09:23 +0000

    render FeaturedConcerts

--- src/pages/concerts.js
+++ src/pages/concerts.js
@@ -1,24 +1,16 @@
-import React from 'react';
+import React from "react"
 
-import Layout from '../components/Layout'
-import HomeHero from '../components/Hero/HomeHero'
-import Concert from '../components/Concerts/Concert';
+import Layout from "../components/Layout"
+import HomeHero from "../components/Hero/HomeHero"
+import FeaturedConcerts from "../components/Concerts/FeaturedConcerts"
 
 const concerts = () => {
-    return (
-        <Layout>
-            <HomeHero />
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <Concert />
-                    </div>
-                    <div className="col"></div>
-                    <div className="col"></div>
-                </div>
-            </div>
-        </Layout>
-    );
+  return (
+    <Layout>
+      <HomeHero />
+      <FeaturedConcerts />
+    </Layout>
+  )
 }
 
-export default concerts;
+export default concerts
