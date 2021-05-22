3ad59446bacf1ddda06494ea51869199b7a692cd
zak <codedpills@gmail.com>
2021-05-22 00:34:35 +0000

    add background image

--- src/components/Login/LoginWrapper.jsx
+++ src/components/Login/LoginWrapper.jsx
@@ -1,12 +1,48 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
-import './login.scss'
+import LoginForm from "./LoginForm"
+
+import "./login.scss"
 
 const LoginWrapper = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      desktop: file(
+        relativePath: {
+          eq: "hero/jordon-conner-tIr-PWgSYB4-unsplash.jpg"
+        }
+      ) {
+        childImageSharp {
+          fluid(quality: 90, maxWidth: 1920) {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+
+  const bgImg = data.desktop.childImageSharp.fluid
+
   return (
     <div className="login-wrapper">
       <div className="row no-gutters">
-        <div className="col-5">bg image</div>
+        <div className="col-5">
+          <div className="background-section">
+            <BackgroundImage
+              fluid={bgImg}
+              background={`#040e18`}
+              style={{ height: "100vh" }}
+            >
+              <div className="bg-text">
+                <h1>When you want to live, you go to concerts!</h1>
+                <br />
+                <p>Photo by Jordon Conner on Unsplash</p>
+              </div>
+            </BackgroundImage>
+          </div>
+        </div>
         <div className="col">
           <div className="login-form-section">
             <div className="logo-area">
@@ -16,7 +52,7 @@ const LoginWrapper = () => {
             <div className="form-area">
               <h4 className="text-center mb-4">Hello, old friend. Sign in</h4>
               <br />
-              Form here
+              <LoginForm />
             </div>
           </div>
         </div>
