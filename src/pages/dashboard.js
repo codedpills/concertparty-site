cd3b582420cf60e5d85aaf21c21863619e32bea9
zak <codedpills@gmail.com>
2021-05-24 13:03:40 +0000

    create the dashboard layout

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
+import Layout from '../components/Layout'
+
 const dashboard = () => {
     return (
-        <div>
-            <h1>Welcome to the Dashboard</h1>
-        </div>
+        <Layout>
+            <div className="container mt-4 mb-4">
+                <h5>MY CONCERT CENTER</h5>
+                <br/>
+                <div className="row">
+                    <div className="col-3">
+                        Profile
+                    </div>
+                    <div className="col">
+                        my concerts
+                    </div>
+                </div>
+            </div>
+        </Layout>
     );
 }
 
