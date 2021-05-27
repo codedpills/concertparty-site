e4283e51f53ca0c72614f225a16d32427ceea3ec
zak <codedpills@gmail.com>
2021-05-27 20:38:56 +0000

    improve user experience by handling edge cases

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -9,6 +9,8 @@ import Concerts from "./Concerts"
 
 const Dashboard = () => {
   const [concerts, setConcerts] = useState([])
+  const [requesting, setRequesting] = useState(true)
+
   useEffect(() => {
     axios({
       method: "get",
@@ -20,10 +22,34 @@ const Dashboard = () => {
     })
       .then(res => {
         setConcerts(res.data.results)
+        setRequesting(false)
       })
-      .catch(err => console.log(err))
+      .catch(err => {
+        console.log(err)
+        setRequesting(false)
+      })
   }, [])
-  console.log("state: ",concerts)
+  console.log("state: ", concerts)
+
+  let allConcerts
+  if (requesting === false && concerts.length > 1) {
+    allConcerts = concerts.map(concert => {
+      const venue =
+        concert.entities.length !== 0 ? concert.entities[0].name : "Venue N/A"
+      const concertInfo = {
+        title: concert.title,
+        startDate: concert.start,
+        endDate: concert.end,
+        tags: concert.labels,
+        venue: venue,
+      }
+      return <Concerts concertInfo={concertInfo} key={concert.id} />
+    })
+  } else if (requesting === false && concerts.length === 0) {
+    allConcerts = <p>Nothing to see here</p>
+  } else {
+    allConcerts = <h5 className="text-center">Loading...</h5>
+  }
 
   return (
     <div className="dashboard shadow mb-5 bg-white rounded p-3">
@@ -33,10 +59,7 @@ const Dashboard = () => {
         </div>
         <Tabs defaultActiveKey="upcoming">
           <Tab eventKey="upcoming" title="Upcoming">
-            <div className="upcoming">
-              <Concerts />
-              <Concerts />
-            </div>
+            <div className="upcoming">{allConcerts}</div>
           </Tab>
           <Tab eventKey="booked" title="Booked">
             <div className="booked">You have no booked concerts yet.</div>
