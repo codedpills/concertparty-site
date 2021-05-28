2dc67163eb724192409db456033b7f5d71255c0f
zak <codedpills@gmail.com>
2021-05-28 12:10:48 +0000

    add meta tags using Helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,24 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 
 import Layout from "../components/Layout"
-import HomeHero from '../components/Hero/HomeHero'
+import HomeHero from "../components/Hero/HomeHero"
 import EmailListFormDisplay from "../components/EmailListForm/EmailListFormDisplay"
 import About from "../components/About/About"
+
+import company from "../images/company/duy-pham-Cecb0_8Hx-o-unsplash.jpg"
 
 const Index = () => {
   return (
     <Layout>
+      <Helmet title="Welcome to ConcertParty">
+        <meta
+          name="description"
+          content="Concert Party is a global community connecting music and show lovers to the most amazing
+concerts around the world."
+        />
+        <meta name="image" content={company} />
+      </Helmet>
       <HomeHero />
       <EmailListFormDisplay />
       <About />
